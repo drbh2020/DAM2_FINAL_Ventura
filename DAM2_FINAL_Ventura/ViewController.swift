f1c4d9288f49c5c05913d5a4526ec60349daab5a
drbh2020 <berastaindiego@gmail.com>
2025-12-27 18:53:59 -0500

    Initial Commit

---
+++ DAM2_FINAL_Ventura/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DAM2_FINAL_Ventura
+//
+//  Created by brstn on 27/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
